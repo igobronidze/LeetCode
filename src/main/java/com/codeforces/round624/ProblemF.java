593ffac6ce92e743711c0b2f7c3250c858c5cb3f
Soso Gobronidze <gobronidze.ioseb@gmail.com>
2020-02-26 11:45:18 +0400

    finished codeforces round 624

--- src/main/java/com/codeforces/round624/ProblemF.java
+++ src/main/java/com/codeforces/round624/ProblemF.java
@@ -3,9 +3,7 @@ package com.codeforces.round624;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ProblemF {
 
@@ -19,19 +17,52 @@ public class ProblemF {
 
         int n = scanner.nextInt();
         List<ProblemF.Pair<Long, Long>> xvList = new ArrayList<>();
+        TreeSet<Long> treeSet = new TreeSet<>();
         for (int i = 0; i < n; i++) {
             xvList.add(new ProblemF.Pair<>(scanner.nextLong(), 0L));
         }
         for (int i = 0; i < n; i++) {
-            xvList.get(i).second = scanner.nextLong();
+            long v = scanner.nextLong();
+            xvList.get(i).second = v;
+            treeSet.add(v);
         }
 
-        
+        Map<Long, Integer> indexesMap = new HashMap<>();
+        int index = treeSet.size() - 1;
+        for (long x : treeSet) {
+            indexesMap.put(x, index--);
+        }
+
+        xvList.sort((a, b) -> {
+            int x = Long.compare(b.first, a.first);
+            if (x != 0) {
+                return x;
+            }
+            return Long.compare(b.second, a.second);
+        });
+
+        BinaryIndexedTree xSumTree = new BinaryIndexedTree(n);
+        BinaryIndexedTree countSumTree = new BinaryIndexedTree(n);
+
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            long x = xvList.get(i).first;
+            long v = xvList.get(i).second;
+
+            index = indexesMap.get(v);
+            ans += (xSumTree.getSum(index) - x * countSumTree.getSum(index));
+
+            xSumTree.update(index, x);
+            countSumTree.update(index, 1);
+        }
+
+        out.println(ans);
 
         out.flush();
     }
 
-    private class BinaryIndexedTree {
+    private static class BinaryIndexedTree {
 
         private long[] bit;
 
@@ -42,7 +73,7 @@ public class ProblemF {
             this.n = n;
         }
 
-        private void update(int index, int delta) {
+        private void update(int index, long delta) {
             index++;
             while (index <= n) {
                 bit[index] += delta;
