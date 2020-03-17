6c86ba94d1b600bddd16fd5f709bc9299008b093
Ioseb Gobronidze <gobronidze.ioseb@gmail.com>
2020-03-17 14:55:00 +0400

    round 612

---
+++ src/main/java/com/codeforces/round612/ProblemC.java
@@ -0,0 +1,95 @@
+package com.codeforces.round612;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ProblemC {
+
+    public static InputStream inputStream = System.in;
+
+    public static OutputStream outputStream = System.out;
+
+    public static void main(String[] args) {
+        MyScanner scanner = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+
+
+        int n = scanner.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            if (x == 0) {
+                list.add(x);
+            } else {
+                if (x % 2 == 0) {
+                    list.add(2);
+                } else {
+                    list.add(1);
+                }
+            }
+        }
+
+
+        out.flush();
+    }
+
+    private static class MyScanner {
+        private BufferedReader bufferedReader;
+        private StringTokenizer stringTokenizer;
+
+        private MyScanner(InputStream inputStream) {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        private String next() {
+            while (stringTokenizer == null || !stringTokenizer.hasMoreElements()) {
+                try {
+                    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return stringTokenizer.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                str = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    private static class Pair<F, S> {
+
+        private F first;
+
+        private S second;
+
+        public Pair() {
+        }
+
+        public Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
