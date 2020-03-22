20892d6042db9d835bb0d1d68e2c9d987400afe4
Ioseb Gobronidze <gobronidze.ioseb@gmail.com>
2020-03-22 14:02:34 +0400

    round 527

---
+++ src/main/java/com/codeforces/round527/ProblemD2.java
@@ -0,0 +1,85 @@
+package com.codeforces.round527;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ProblemD2 {
+
+    public static InputStream inputStream = System.in;
+
+    public static OutputStream outputStream = System.out;
+
+    public static void main(String[] args) {
+        MyScanner scanner = new MyScanner(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+
+        int n = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+
+
+
+        out.flush();
+    }
+
+    private static class MyScanner {
+        private BufferedReader bufferedReader;
+        private StringTokenizer stringTokenizer;
+
+        private MyScanner(InputStream inputStream) {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        private String next() {
+            while (stringTokenizer == null || !stringTokenizer.hasMoreElements()) {
+                try {
+                    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return stringTokenizer.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                str = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    private static class Pair<F, S> {
+
+        private F first;
+
+        private S second;
+
+        public Pair() {}
+
+        public Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
